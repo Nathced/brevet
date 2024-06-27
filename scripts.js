77e1d4dfd8d882175e1ce69a0b540a953d89c683
Nathced <156846829+Nathced@users.noreply.github.com>
2024-06-27 12:27:39 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,40 @@
+// scripts.js
+let timer;
+let isBreak = true;
+
+document.getElementById('startPause').addEventListener('click', function() {
+    startTimer(15 * 60, 'pause');
+});
+
+function startTimer(duration, type) {
+    let timerDisplay = document.getElementById('timerDisplay');
+    let minutes, seconds;
+    clearInterval(timer);
+    timer = setInterval(function() {
+        minutes = parseInt(duration / 60, 10);
+        seconds = parseInt(duration % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        timerDisplay.textContent = minutes + ":" + seconds;
+
+        if (--duration < 0) {
+            clearInterval(timer);
+            document.getElementById('alarmSound').play();
+            if (type === 'pause') {
+                if (confirm("Veux-tu commencer une session de révision de 45 minutes ?")) {
+                    startTimer(45 * 60, 'revision');
+                } else {
+                    startTimer(15 * 60, 'pause');
+                }
+            } else {
+                if (confirm("Veux-tu prendre une pause de 15 minutes ?")) {
+                    startTimer(15 * 60, 'pause');
+                } else {
+                    startTimer(45 * 60, 'revision');
+                }
+            }
+        }
+    }, 1000);
+}
